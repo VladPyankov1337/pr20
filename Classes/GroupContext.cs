de2ded5b7785eec34c83a3e3c89bcaceaf44112d
VladPyankov1337 <vladipyankov@yandex.ru>
2025-11-05 00:50:30 +0500

    Разработанные контексты данных для работы с БД. Пьянков

--- Classes/GroupContext.cs
+++ Classes/GroupContext.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using MySql.Data.MySqlClient;
+using ReportGeneration.Classes.Common;
+using ReportGeneration.Models;
+using System.Collections.Generic;
+
+namespace ReportGeneration.Classes
+{
+    public class GroupContext : Group
+    {
+        public GroupContext(int Id, string Name) : base(Id, Name) { }
+        public static List<GroupContext> AllGroups()
+        {
+            List<GroupContext> allGroups = new List<GroupContext>();
+            MySqlConnection connection = Connection.OpenConnection();
+            MySqlDataReader BDGroups = Connection.Query("SELECT * FROM `group` ORDER BY `Name`", connection);
+            while (BDGroups.Read())
+            {
+                allGroups.Add(new GroupContext(
+                    BDGroups.GetInt32(0),
+                    BDGroups.GetString(1)
+                    ));
+            }
+            Connection.CloseConnection(connection);
+            return allGroups;
+        }
+    }
+}
--- Classes/StudentContext.cs
+++ Classes/StudentContext.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using MySql.Data.MySqlClient;
+using ReportGeneration.Classes.Common;
+using ReportGeneration.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ReportGeneration.Classes
+{
+    public class StudentContext : Student
+    {
+        public StudentContext(int id, string firstName, string lastName, int idGroup, bool expelled, DateTime dateExpelled) : base(id, firstName, lastName, idGroup, expelled, dateExpelled) { }
+
+        public static List<StudentContext> AllStudents()
+        {
+            List<StudentContext> allStudents = new List<StudentContext>();
+            MySqlConnection connection = Connection.OpenConnection();
+            MySqlDataReader BDStudents = Connection.Query("SELECT * FROM `student` ORDER BY `LastName`", connection);
+            while (BDStudents.Read())
+            {
+                allStudents.Add(new StudentContext(
+                    BDStudents.GetInt32(0),
+                    BDStudents.GetString(1),
+                    BDStudents.GetString(2),
+                    BDStudents.GetInt32(3),
+                    BDStudents.GetBoolean(4),
+                    BDStudents.IsDBNull(5) ? DateTime.Now : BDStudents.GetDateTime(5)
+                    ));
+            }
+            Connection.CloseConnection(connection);
+            return allStudents;
+        }
+    }
+}
