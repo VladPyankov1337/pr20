84f049733111953eb1904dd3c7e8cc2556862605
VladPyankov1337 <vladipyankov@yandex.ru>
2025-11-05 01:18:09 +0500

    Программирование окна Main. Пьянков

--- Items/Student.xaml.cs
+++ Items/Student.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using ReportGeneration.Classes;
+using ReportGeneration.Pages;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ReportGeneration.Items
 {
@@ -20,7 +9,7 @@ namespace ReportGeneration.Items
     /// </summary>
     public partial class Student : UserControl
     {
-        public Student()
+        public Student(StudentContext student, Main main)
         {
             InitializeComponent();
         }
--- Pages/Main.xaml.cs
+++ Pages/Main.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
+﻿using ReportGeneration.Classes;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ReportGeneration.Pages
 {
@@ -20,9 +10,51 @@ namespace ReportGeneration.Pages
     /// </summary>
     public partial class Main : Page
     {
+        public List<GroupContext> AllGroups = GroupContext.AllGroups();
+        public List<StudentContext> AllStudents = StudentContext.AllStudents();
+        public List<WorkContext> AllWorks = WorkContext.AllWorks();
+        public List<EvaluationContext> AllEvaluations = EvaluationContext.AllEvaluations();
+        public List<DisciplineContext> AllDisciplines = DisciplineContext.AllDisciplines();
+
         public Main()
         {
             InitializeComponent();
+            CreateGroupUI();
+            CreateStudents(AllStudents);
+        }
+        public void CreateGroupUI()
+        {
+            foreach (GroupContext Group in AllGroups)
+                CBGroups.Items.Add(Group.Name);
+            CBGroups.Items.Add("Выберите");
+            CBGroups.SelectedIndex = CBGroups.Items.Count - 1;
+        }
+
+        public void CreateStudents(List<StudentContext> AllStudents)
+        {
+            Parent.Children.Clear();
+            foreach (StudentContext Student in AllStudents)
+                Parent.Children.Add(new Items.Student(Student, this));
+        }
+
+        private void SelectGroup(object sender, SelectionChangedEventArgs e)
+        {
+            if (CBGroups.SelectedIndex != CBGroups.Items.Count - 1)
+            {
+                int IdGroup = AllGroups.Find(x => x.Name == CBGroups.SelectedItem).Id;
+                CreateStudents(AllStudents.FindAll(x => x.IdGroup == IdGroup));
+            }
+        }
+
+        private void SelectStudents(object sender, KeyEventArgs e)
+        {
+            List<StudentContext> SearchStudent = AllStudents;
+            if (CBGroups.SelectedIndex != CBGroups.Items.Count - 1)
+            {
+                int IdGroup = AllGroups.Find(x => x.Name == CBGroups.SelectedItem).Id;
+                SearchStudent = AllStudents.FindAll(x => x.IdGroup == IdGroup);
+            }
+            CreateStudents(SearchStudent.FindAll(x => $"{x.LastName} {x.FirstName}".Contains(TBFio.Text)));
         }
     }
 }
