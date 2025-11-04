93b0c9d9c6682d193050132a871a9941ce83c1ab
VladPyankov1337 <vladipyankov@yandex.ru>
2025-11-05 00:25:15 +0500

    Создание структуры проекта

--- Models/Group.cs
+++ Models/Group.cs
@@ -1,13 +1,1 @@
-﻿namespace ReportGeneration.Models
-{
-    public class Group
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public Group(int Id, string Name)
-        {
-            this.Id = Id;
-            this.Name = Name;
-        }
-    }
-}
+﻿
